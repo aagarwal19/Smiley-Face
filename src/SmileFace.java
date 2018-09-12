2e1e6e041ab59dbb19620cb6a1bdbb6001bec7ec
Arun Agarwal <42978471+aagarwal19@users.noreply.github.com>
2018-09-12 14:07:16 -0400

    Add files via upload

---
+++ src/SmileFace.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Arun (Blaze) Agarwal
+ * Smiley Face Pd. 7
+ *
+ */
+
+public class SmileFace 
+{
+
+	public static void main(String[] args) 
+	{
+	System.out.println("\\\\\\\\\\\\\\\\\\");
+	System.out.println("| *   * |");
+	System.out.println("|   *   |");
+	System.out.println("| \\___/ |");
+	System.out.println("\\ __ __ / ");
+	}
+
+}
